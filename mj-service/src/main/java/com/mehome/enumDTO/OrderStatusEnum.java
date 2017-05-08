d376f7b82809dd59a097b78cd0ee369e4f4f5004
xuyixun21 <624200756@qq.com>
2017-05-08 18:04:57 +0800

    定义订单状态枚举类

--- mj-service/src/main/java/com/mehome/enumDTO/OrderStatusEnum.java
+++ mj-service/src/main/java/com/mehome/enumDTO/OrderStatusEnum.java
@@ -3,7 +3,7 @@ package com.mehome.enumDTO;
 public enum OrderStatusEnum {
 
 //0预约   1确认中 2已经确认 3入住  4结束 5取消
-	ORDER(0, "预约"), CONFIRMING(1,"确认中"),CONFIRMED(2, "已经确认");
+	ORDER(0, "预约"), CONFIRMING(1,"确认中"),CONFIRMED(2, "已经确认"),CHECKIN(3,"入住"),END(4,"结束"),CANCEL(5,"结束");
 
     private int key;
     private String value;
