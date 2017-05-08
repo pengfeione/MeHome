6f377c3a364989547cf0f935c0e4a0d48e1e78d9
xuyixun21 <624200756@qq.com>
2017-05-08 18:16:57 +0800

    更改

---
+++ mj-service/src/main/java/com/mehome/requestDTO/BasicBean.java
@@ -0,0 +1,16 @@
+package com.mehome.requestDTO;
+
+public class BasicBean {
+
+	private Integer basicId;
+	
+	private String pic;
+	
+	private String basicName;
+	
+	private String basicType;
+	
+	private String updateTime;
+	
+	private Boolean basicActive;
+}
