c8f5575404396b814f902be64d141fc5bbf8f6fb
renhui <337375464@qq.com>
2017-05-15 09:35:28 +0800

    新增后台页面·1

---
+++ com.mehome.service/src/main/java/com/mehome/config/GenericCacheConfig.java
@@ -0,0 +1,14 @@
+package com.mehome.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2017/5/12.
+ */
+@Configuration
+public class GenericCacheConfig {
+
+
+
+}
