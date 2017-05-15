8b1c21911bd5b0512d077ae8048ba70e8171965b
彭飞 <pengfei@pengfeis-MacBook-Pro.local>
2017-05-15 15:59:59 +0800

    提交unit

--- com.mehome.service/src/test/java/com/mehome/TestSend.java
+++ com.mehome.service/src/test/java/com/mehome/TestSend.java
@@ -5,12 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Created by pengfei on 2017/5/15.
+ */
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceApplicationTests {
+public class TestSend {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
 
+        System.out.println("2222");
+    }
 }
