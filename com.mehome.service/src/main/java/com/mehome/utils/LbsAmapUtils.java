82f1ffb4e24d3bb3079240921b59f102477cc714
xuyixun21 <624200756@qq.com>
2017-05-23 13:16:50 +0800

    高德地图工具类以及http工具类

---
+++ com.mehome.service/src/main/java/com/mehome/utils/LbsAmapUtils.java
@@ -0,0 +1,14 @@
+package com.mehome.utils;
+/**
+ * 高德地图web api工具类
+ * @author xuyixun21
+ *
+ */
+public class LbsAmapUtils {
+	private static String mapUrl="http://restapi.amap.com/v3/place/text?parameters";
+	private static String mapKey="2e174be6175a21db94251aa6c2542d90";
+	
+	public static void main(String[] args) {
+		
+	}
+}
