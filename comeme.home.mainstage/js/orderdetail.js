391a5df170132c7bd48ba13a104cc092ecd38250
xuyixun21 <624200756@qq.com>
2017-06-21 17:14:23 +0800

    更改

--- comeme.home.mainstage/js/orderdetail.js
+++ comeme.home.mainstage/js/orderdetail.js
@@ -22,13 +22,28 @@ $("#offline_cancel").click(function() {
 	$("#order_offline").fadeOut();
 });
 $("#offline_sure").click(function() {
-	$("#order_offline").fadeOut();
-	$("#already_deposit").attr("class", "depositInit");
-	// alert($("#already_deposit").attr("class"));
-	$("#em").hide();
+	var orderId = util.getUrlParam("orderId");
+	var url = util.baseUrl + "/order/updateOrder";
+	var param ='{"orderId":'+orderId+',"platformHost":'+false+'}';
+	var contentType = "application/json";
+	util.requestRemoteDataJsonPosta(url,param,contentType,function(data){
+		$("#order_offline").fadeOut();
+		$("#already_deposit").attr("class", "depositInit");
+		// alert($("#already_deposit").attr("class"));
+		$("#em").hide();
+	});
 
 });
 $("#pay").click(function() {
-	$("#pay_choose").fadeOut();
-	$("#already_deposit").attr("class", "depositSelect");
+	/*$("#pay_choose").fadeOut();
+	$("#already_deposit").attr("class", "depositSelect");*/
+	//请求支付接口
+	var orderId = util.getUrlParam("orderId"); 
+	var url = util.baseUrl + "/order/updateOrder";
+	var param ='{"orderId":'+orderId+',"payOnline":'+true+',"platformHost":'+true+',"payType":"wechat"}';
+	var contentType = "application/json";
+	util.requestRemoteDataJsonPosta(url,param,contentType,function(data){
+		$("#pay_choose").fadeOut();
+		$("#already_deposit").attr("class", "depositSelect");
+	});
 });
