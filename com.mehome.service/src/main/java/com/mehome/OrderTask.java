6538c7f8a47fda71da22326adbc37863ccc43521
xuyixun21 <624200756@qq.com>
2017-07-18 10:04:16 +0800

    更改

--- com.mehome.service/src/main/java/com/mehome/OrderTask.java
+++ com.mehome.service/src/main/java/com/mehome/OrderTask.java
@@ -3,6 +3,7 @@ package com.mehome;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,21 +14,25 @@ import com.mehome.service.iface.IOrderService;
 import com.mehome.utils.DateUtils;
 @Component
 public class OrderTask {
+	private Logger log = Logger.getLogger(this.getClass());
 	@Autowired
     private IOrderService orderService;
-	 //每日凌晨 更新已经确认的订单  是否到达入住时间   以及 结束时间，进行对应状态的更新
-    @Scheduled(cron="0 0 0  * * ? ")
+	 //每日凌晨 更新已经确认的订单  是否到达入住时间   以及 结束时间，进行对应状态的更新   @Scheduled(cron="0 0 1  * * ? ")
+    @Scheduled(cron="0 0/5 * * * ?")
     public void updateOrderStatus(){
+    	log.info("触发更新订单状态进程");
     	Date date = new Date();
-    	Date compareDate= DateUtils.getToDayStart(date);
+    	Date compareDate= date;
     	OrderBean bean=new OrderBean();
     	bean.setOrderStatus(OrderStatusEnum.CONFIRMED.getKey());
     	List<OrderBean> list=orderService.getListByCondition(bean);
     	for (OrderBean orderBean : list) {
     		String startTime=orderBean.getStartTime();
+    		String endTime=orderBean.getEndTime();
     		//更新为入住状态
     		Date start=DateUtils.strToDate(startTime);
-    		if(start.compareTo(compareDate)==0){
+    		Date end=DateUtils.strToDate(endTime);
+    		if(compareDate.after(start)&&compareDate.before(end)){
     			OrderBean updateBean=new OrderBean();
     			updateBean.setOrderId(orderBean.getOrderId());
     			updateBean.setOrderStatus(OrderStatusEnum.CHECKIN.getKey());
@@ -41,7 +46,7 @@ public class OrderTask {
     		String endTime=orderBean.getEndTime();
     		//更新为结束状态
     		Date end=DateUtils.strToDate(endTime);
-    		if(end.compareTo(compareDate)==0){
+    		if(end.before(compareDate)){
     			OrderBean updateBean=new OrderBean();
     			updateBean.setOrderId(orderBean.getOrderId());
     			updateBean.setOrderStatus(OrderStatusEnum.END.getKey());
