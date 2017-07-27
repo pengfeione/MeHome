291a0a371e6cfba1b484bc2a5debd694d5393f45
renhui <337375464@qq.com>
2017-07-27 13:20:15 +0800

    修改房源的上下线状态

--- com.mehome.service/src/main/java/com/mehome/enumDTO/HouseStatusEnum.java
+++ com.mehome.service/src/main/java/com/mehome/enumDTO/HouseStatusEnum.java
@@ -3,7 +3,7 @@ package com.mehome.enumDTO;
 public enum HouseStatusEnum {
 
 	// 1可用、2已租借、0已下线
-	OFFLINE(0, "已下线"), AVAILABLE(1, "可用"), LEASED(2, "已租借");
+	OFFLINE(0, "已下线"), AVAILABLE(1, "上线"), LEASED(2, "已租借");
 
 	private int key;
 	private String value;
